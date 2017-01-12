f91be88344d525b87fc468e5a45e07ec9599e526
Alexander Scheurer <mail@aspepex.net>
2017-01-12 10:34:36 +0100

    stuff

---
+++ Assets/Stuff.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class Stuff : MonoBehaviour
+{
+    private float focusUpdateEvery = 5f;
+    private float focusTimer;
+
+    void Update()
+    {
+        if (focusTimer <= 0)
+        {
+            focusTimer = focusUpdateEvery;
+            Focus();
+        }
+        else
+        {
+            focusTimer -= Time.deltaTime;
+        }
+    }
+
+    private void Focus()
+    {
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+    }
+}
